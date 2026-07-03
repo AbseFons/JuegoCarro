a2ef7fd4482e7dac00053d0cfc22e03979e0297f
AbseFons <abner.fonseca.melgarejo@gmail.com>
2026-07-03 00:24:53 -0500

    fix game

--- juego.js
+++ juego.js
@@ -5,15 +5,40 @@ var enemigos;
 var timer;
 var gasolinas;
 var timerGasolina;
-var puntos;      
-var txtPuntos;   
+
+var puntos;
+var txtPuntos;
+
+var vidas;
+var txtVidas;
+
+var nivel;
+var txtNivel;
+
+var gasolinasConsumidas;
+
+var velocidadFondo;
+var velocidadEnemigo;
+var velocidadGasolina;
+
+var sonidoChoque;
+var sonidoPunto;
+var musicaFondo;
+
+var invulnerable = false;
 
 var Juego = {
     preload: function(){
         juego.load.image('bg', 'img/bg.png');
         juego.load.image('carro', 'img/carro.png');
         juego.load.image('carroMalo', 'img/carroMalo.png');
+        juego.load.image('carroMalo1', 'img/carroMalo1.png');
         juego.load.image('gasolina', 'img/gas.png');
+
+        juego.load.audio('choque', 'audio/choque.mp3');
+        juego.load.audio('punto', 'audio/punto.mp3');
+        juego.load.audio('fondoMusica', 'audio/fondo.mp3');
+
         juego.forceSingleUpdate = true;
     },
 
@@ -21,14 +46,12 @@ var Juego = {
         juego.physics.startSystem(Phaser.Physics.ARCADE);
 
         fondo = juego.add.tileSprite(0, 0, 290, 540, 'bg');
-        
-        carro = juego.add.sprite(juego.width/2, 496, 'carro');
+
+        carro = juego.add.sprite(juego.width / 2, 496, 'carro');
         carro.anchor.setTo(0.5);
         juego.physics.arcade.enable(carro);
 
-        // Enemigos
         enemigos = juego.add.group();
-        juego.physics.arcade.enable(enemigos, true);
         enemigos.enableBody = true;
         enemigos.createMultiple(20, 'carroMalo');
         enemigos.setAll('anchor.x', 0.5);
@@ -36,9 +59,7 @@ var Juego = {
         enemigos.setAll('outOfBoundsKill', true);
         enemigos.setAll('checkWorldBounds', true);
 
-        // Gasolinas
         gasolinas = juego.add.group();
-        juego.physics.arcade.enable(gasolinas, true);
         gasolinas.enableBody = true;
         gasolinas.createMultiple(20, 'gasolina');
         gasolinas.setAll('anchor.x', 0.5);
@@ -46,63 +67,149 @@ var Juego = {
         gasolinas.setAll('outOfBoundsKill', true);
         gasolinas.setAll('checkWorldBounds', true);
 
-        // Timers
-        timer = juego.time.events.loop(1500, this.crearCarroMalo, this);
-        timerGasolina = juego.time.events.loop(2000, this.crearGasolina, this);
-
         cursores = juego.input.keyboard.createCursorKeys();
 
-        
         puntos = 0;
-        txtPuntos = juego.add.text(20, 20, "0", { font: "30px Arial", fill: "#FFF", fontWeight: "bold" });
+        vidas = 3;
+        nivel = 1;
+        gasolinasConsumidas = 0;
+
+        velocidadFondo = 3;
+        velocidadEnemigo = 200;
+        velocidadGasolina = 200;
+
+        txtPuntos = juego.add.text(15, 15, "Puntos: 0", {
+            font: "20px Arial",
+            fill: "#FFF",
+            fontWeight: "bold"
+        });
+
+        txtVidas = juego.add.text(15, 45, "Vidas: 3", {
+            font: "20px Arial",
+            fill: "#FFF",
+            fontWeight: "bold"
+        });
+
+        txtNivel = juego.add.text(15, 75, "Nivel: 1", {
+            font: "20px Arial",
+            fill: "#FFF",
+            fontWeight: "bold"
+        });
+
+        sonidoChoque = juego.add.audio('choque');
+        sonidoPunto = juego.add.audio('punto');
+        musicaFondo = juego.add.audio('fondoMusica');
+
+        musicaFondo.loopFull(0.4);
+
+        timer = juego.time.events.loop(1500, this.crearCarroMalo, this);
+        timerGasolina = juego.time.events.loop(2000, this.crearGasolina, this);
     },
 
     update: function(){
-        fondo.tilePosition.y += 3;
+        fondo.tilePosition.y += velocidadFondo;
 
-        // Movimiento
         if (cursores.right.isDown && carro.position.x < 245) {
             carro.position.x += 5;
         } else if (cursores.left.isDown && carro.position.x > 45) {
             carro.position.x -= 5;
         }
 
-        // Detección de Colisiones
         juego.physics.arcade.overlap(carro, enemigos, this.chocarEnemigo, null, this);
-        
-        // Colisión beneficiosa (Puntaje)
         juego.physics.arcade.overlap(carro, gasolinas, this.cogerGasolina, null, this);
     },
 
     crearCarroMalo: function(){
         var posicion = Math.floor(Math.random() * 3) + 1;
         var enemigo = enemigos.getFirstDead();
-        if(enemigo){
-            enemigo.physicsBodyType = Phaser.Physics.ARCADE;
+
+        if (enemigo) {
             enemigo.reset(posicion * 73, 0);
-            enemigo.body.velocity.y = 200;
-            enemigo.anchor.setTo(0.5);
+
+            if (nivel === 2) {
+                enemigo.loadTexture('carroMalo1');
+            } else {
+                enemigo.loadTexture('carroMalo');
+            }
+
+            enemigo.body.velocity.y = velocidadEnemigo;
         }
     },
 
     crearGasolina: function(){
         var posicion = Math.floor(Math.random() * 3) + 1;
         var gasolina = gasolinas.getFirstDead();
-        if(gasolina){
-            gasolina.physicsBodyType = Phaser.Physics.ARCADE;
+
+        if (gasolina) {
             gasolina.reset(posicion * 73, 0);
-            gasolina.body.velocity.y = 200;
-            gasolina.anchor.setTo(0.5);
+            gasolina.body.velocity.y = velocidadGasolina;
         }
     },
 
-    chocarEnemigo: function(){
-        juego.state.start('Terminado');
+    chocarEnemigo: function(jugador, enemigo){
+        if (invulnerable) {
+            return;
+        }
+
+        enemigo.kill();
+        vidas--;
+        txtVidas.text = "Vidas: " + vidas;
+
+        sonidoChoque.play();
+
+        invulnerable = true;
+        carro.alpha = 0.5;
+
+        juego.time.events.add(1000, function(){
+            invulnerable = false;
+            carro.alpha = 1;
+        }, this);
+
+        if (vidas <= 0) {
+            musicaFondo.stop();
+            juego.state.start('Terminado');
+        }
     },
 
-    cogerGasolina: function(carro, gasolina){
-        gasolina.kill(); 
+    cogerGasolina: function(jugador, gasolina){
+        gasolina.kill();
+
         puntos += 10;
-        txtPuntos.text = puntos;
+        gasolinasConsumidas++;
+
+        txtPuntos.text = "Puntos: " + puntos;
+
+        sonidoPunto.play();
+
+        if (gasolinasConsumidas >= 3 && nivel === 1) {
+            this.pasarNivelDos();
+        }
+    },
+
+    pasarNivelDos: function(){
+        nivel = 2;
+        txtNivel.text = "Nivel: 2";
+
+        velocidadFondo = 5;
+        velocidadEnemigo = 300;
+        velocidadGasolina = 300;
+
+        juego.time.events.remove(timer);
+        juego.time.events.remove(timerGasolina);
+
+        timer = juego.time.events.loop(1000, this.crearCarroMalo, this);
+        timerGasolina = juego.time.events.loop(1700, this.crearGasolina, this);
+
+        var aviso = juego.add.text(juego.width / 2, juego.height / 2, "NIVEL 2", {
+            font: "38px Arial",
+            fill: "#FFD700",
+            fontWeight: "bold"
+        });
+
+        aviso.anchor.setTo(0.5);
+
+        juego.time.events.add(1200, function(){
+            aviso.destroy();
+        }, this);
     }
 };
--- terminado.js
+++ terminado.js
@@ -1,10 +1,19 @@
 var Terminado = {
     create: function() {
-        var mensaje = "Ha perdido. Su puntaje final fue: " + puntos + "\n\n¿Desea reiniciar?";
-        var reiniciar = confirm(mensaje);
-        
-        if (reiniciar) {
+        var mensaje = "Ha perdido.\nPuntaje final: " + puntos + "\n\nPresione ENTER para reiniciar";
+
+        var texto = juego.add.text(juego.width / 2, juego.height / 2, mensaje, {
+            font: "22px Arial",
+            fill: "#FFF",
+            align: "center",
+            fontWeight: "bold"
+        });
+
+        texto.anchor.setTo(0.5);
+
+        var teclaEnter = juego.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+        teclaEnter.onDown.add(function(){
             juego.state.start('Juego');
-        }
+        }, this);
     }
 };
